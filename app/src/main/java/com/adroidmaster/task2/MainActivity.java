03a0390cd2285dceb668867239fb6a8f6f578c85
adroidmasters <adroidmasters5437@gmail.com>
2022-01-13 18:06:08 +0530

    task2

---
+++ app/src/main/java/com/adroidmaster/task2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.adroidmaster.task2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.home:
+                        Toast.makeText(MainActivity.this, "home option", Toast.LENGTH_SHORT).show();
+                    case R.id.setting:
+                        Toast.makeText(MainActivity.this, "setting option", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+
+
+
+        });
+    }
+}
